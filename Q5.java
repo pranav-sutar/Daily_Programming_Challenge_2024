f6abffd62827e97c013ad628c69a9163518a113b
Pranav Sutar <pranavssutar@gmail.com>
2024-09-13 20:31:12 +0530

    Q5 To find Leader

---
+++ Q5.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q5 {
+    public static List<Integer> findLeaders(int[] arr) {
+        int n = arr.length;
+        List<Integer> leaders = new ArrayList<>();
+        
+        int maxFromRight = arr[n - 1];
+        leaders.add(maxFromRight);
+        
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > maxFromRight) {
+                leaders.add(arr[i]);
+                maxFromRight = arr[i];
+            }
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        for (int i = leaders.size() - 1; i >= 0; i--) {
+            result.add(leaders.get(i));
+        }
+        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+        List<Integer> leaders = findLeaders(arr);
+        System.out.println("Leaders: " + leaders);
+    }
+}
