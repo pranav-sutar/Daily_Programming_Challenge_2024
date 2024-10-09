01acd0ea78353d4cf178c4ab4e1b0a74505fe9a7
Pranav Sutar <pranavssutar@gmail.com>
2024-10-09 08:28:30 +0530

    Q28

---
+++ Q28.java
@@ -0,0 +1,37 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Q28 {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+
+    public static void main(String[] args) {
+        // Construct the binary tree from the given input
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        Q28 solution = new Q28();
+        boolean isSymmetric = solution.isSymmetric(root);
+        System.out.println("Is the binary tree symmetric? " + isSymmetric);
+    }
+}
